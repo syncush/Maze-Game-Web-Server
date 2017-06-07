6d18ca6b965f4047f0544fba85ff3876e0613ef9
sync <danielhermonsync@gmail.com>
2017-06-07 13:45:24 +0300

    wdasdasd

---
+++ WebServer/wwwroot/js/Plugin.MazeBoard.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+    $.fn.greenify = function () {
+        this.css("color", "green");
+        return this;
+    };
+    $.fn.drawMaze = function (maze) {
+        var myCanvas = document.getElementById("mazeCanvas");
+        var context = mazeCanvas.getContext("2d");
+        var rows = maze.length;
+        var cols = maze[0].length;
+        var cellWidth = myCanvas.width / cols;
+        var cellHeight = myCanvas.height / rows;
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                if (maze[i][j] == 1) {
+                    context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                }
+            }
+        }
+    };
+}(jQuery));
---
+++ WebServer/wwwroot/js/Singleplayer.js
@@ -0,0 +1,63 @@
+﻿jQuery(function ($)  {
+    var maze = [[0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]];
+    var currX = 0;
+    var currY = 0;
+    var mazeRows = maze.length;
+    var mazeCols = maze[0].length;
+
+    var c = document.getElementById("mazeCanvas");
+    var canvasFX = c.getContext("2d");
+
+    var playerImg = document.getElementById("playImg");
+    var endGameImg = document.getElementById("endImage");
+
+    $("#myNavbar").load("HomePage.html");
+    $("#playForm").on("submit",function( e ) {
+		e.preventDefault();
+        var canv = document.getElementById('mazeCanvas');
+        canv.width = 50 * mazeCols;
+        canv.height = 50 * mazeRows;
+        $("#mazeCanvas").drawMaze(maze);
+    });
+
+    $("body").on("keydown", function (e) {
+        // Handle the arrow keys
+        switch (e.which) {
+            case 37:
+                if (currX >= 0 && currX < mazeRows && currY - 1 < mazeCols && currY >= 0 && maze[currX][currY - 1] == 0) {
+                    alert("left");
+                    currY = currY - 1;
+                }
+                break;
+            case 38:
+                if (currX - 1 >= 0 && currX - 1 < mazeRows && currY < mazeCols && currY >= 0 && maze[currX - 1][currY] == 0) {
+                    currX = currX - 1;
+                    alert("up");
+                }
+                break;
+            case 39:
+                if (currX >= 0 && currX < mazeRows && currY + 1 < mazeCols && currY + 1 >= 0 && maze[currX][currY + 1] == 0) {
+                    alert("right");
+                    currY = currY + 1;
+                }
+                break;
+            case 40:
+                if (currX + 1 >= 0 && currX + 1 < mazeRows && currY < mazeCols && currY >= 0 && maze[currX + 1][currY] == 0) {
+                    alert("down");
+                    currX = currX + 1;
+                }
+                break;
+        }
+    });
+    function OnValueInitStartUp() {
+        $('input[name="Cols"]').val(localStorage.getItem('Colsz'));
+        $('input[name="Rows"]').val(localStorage.getItem('Rowz'));
+        $("#AlgoritmComboBox").val((localStorage.getItem('AlgoSelected')));
+    }
+
+});
