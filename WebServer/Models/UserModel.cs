7dfdc8a450711096ea41f7fa6e71112d05980440
sync <danielhermonsync@gmail.com>
2017-06-10 16:42:33 +0300

    Data connection worked!

--- WebServer/Models/UserModel.cs
+++ WebServer/Models/UserModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using WebServer.Entitys;
+using System.Data.OleDb;
 
 namespace WebServer.Models
 {
@@ -15,12 +17,26 @@ namespace WebServer.Models
         /// The users
         /// </summary>
         public static List<Tuple<string, string, string>> users = new List<Tuple<string, string, string>>();
+        private static DataTable dataTable;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UserModel"/> class.
         /// </summary>
         public UserModel() {
-            
+            try
+            {
+                OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\Users\sync\Desktop\WebServer\WebServer\MazeGameDB.mdb");
+                OleDbCommand dCommand = new OleDbCommand("Select * from Users", con);
+                OleDbDataAdapter adapter = new OleDbDataAdapter(dCommand);
+                dataTable = new DataTable("Users");
+                adapter.Fill(dataTable);
+                con.Close();
+            } catch(Exception e)
+            {
+
+            }
+           
+
         }
 
         /// <summary>
@@ -28,6 +44,7 @@ namespace WebServer.Models
         /// </summary>
         /// <param name="user">The user.</param>
         public void Register(NewUser user) {
+            
             users.Add(new Tuple<string, string, string>(user.UserName, user.Password, user.Email));
         }
 
