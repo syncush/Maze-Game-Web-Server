74d6e0d01859692200aae9ff3e23ac045d275885
sync <danielhermonsync@gmail.com>
2017-06-15 15:54:03 +0300

    sdfs

--- MazeWebServer/js/HomePage.js
+++ MazeWebServer/js/HomePage.js
@@ -3,15 +3,21 @@
     function MoveForm(form) {
         $("#palapa").load(form);
     }
+    
     $(function () {
         var isUser = sessionStorage.getItem('user');
-        if (isUser != undefined && isUser == null) {
+        if (isUser == null) {
             //TODO sett attr
         }
     });
     $("#regist").on("click", function () {
         if (!isLogged) {
             MoveForm("Register.html");
+        } else {
+            sessionStorage.clear();
+            isLogged = false;
+            data = null;
+            MoveForm("index.html");
         }
     });
     $("#login").on("click", function () {
@@ -19,11 +25,30 @@
             MoveForm("Login.html");
         }
     });
+    var refreshIntervalId = setInterval(function () {
+        if (data != null) {
+            data = sessionStorage.getItem('user');
+            if (data != null) {
+                break;
+            } else {
+                isLogged = false;
+                sessionStorage.clear();
+                $("#regist").html("Register");
+                $("#login").html("Login");
+            }
+        }
+        
+    }, 1000);
     $("#settings").on("click", function () {
         MoveForm("Settings.html");
     });
     $("#Multiplayer").on("click", function () {
+        if (isLogged) {
             MoveForm("MultiPlayer.html");
+        } else {
+            MoveForm("Login.html");
+        }
+            
     });
     $("#Singleplayer").on("click", function () {
             MoveForm("SinglePlayer.html");
@@ -32,5 +57,4 @@
             MoveForm("index.html");
     });
     $("#palapa").load("mainPage.html");
-
 });
